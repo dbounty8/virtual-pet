8284ba7ecf13430c6f4a4cda1ce3b2e466442c54
Anthony <anthonyw22@gmail.com>
2018-02-28 22:16:02 +0000

    added the pet age method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,3 +14,11 @@ describe('constructor', () => {
   expect(pet.age).toEqual(0);
   });
 });
+
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  })
+});
